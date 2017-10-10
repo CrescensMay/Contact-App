9c5bf9c9f41616432cdf70bcd03b2165eb46beec
Fidele <crescens.kob@outlook.fr>
2017-10-10 23:40:11 +0800

    Sorting contacts according their firstname, by using extensions

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
+// MARK: Sorting
+
+extension Contact {
+    var firstLetterForSort: String {
+        return String(firstName.characters.first!).uppercased()
+    }
+}
+
+extension ContactsSource {
+    static var sortedUniqueFirstLetter: [String] {
+        let firstLetters = contacts.map {
+            $0.firstLetterForSort
+        }
+        let uniqueFirstLetters = Set(firstLetters)
+        
+        return Array(firstLetters).sorted()
+    }
+    
+    static var sectionContacts: [[Contact]] {
+        let sections = sortedUniqueFirstLetter.map {
+            firstLetter in let filteredContacts = contacts.filter {
+                $0.firstLetterForSort == firstLetter
+            }
+            return filteredContacts.sorted(by: {$0.firstName < $1.firstName})
+        }
+    }
+}
+
 class ContactListController: UITableViewController {
     
     //getting some data
